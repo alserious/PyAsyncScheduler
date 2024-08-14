5876cd3c04060e6041c556de62c63157d154f7ac
alserious <alseriousoff@gmail.com>
2024-08-14 04:32:39 -0700

    add core logic

--- PyAsyncScheduler/__init__.py
+++ PyAsyncScheduler/__init__.py
@@ -1,6 +1,9 @@
+import cProfile
 import logging
 import sys
+import time
 from functools import wraps
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 # import asyncio
 from dataclasses import dataclass
@@ -21,7 +24,10 @@ from typing import Optional, Callable, Union, Coroutine
 from collections import deque
 # import collections
 
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)s-%(asctime)s-%(message)s")
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s-%(asctime)s-%(process)s-%(thread)s-%(message)s",
+)
 logger = logging.getLogger("Async Scheduler")
 
 
@@ -30,9 +36,10 @@ class Job:
     type: str
     when: int
     status: str
-    func: Callable
+    func: Callable | Coroutine
     args: tuple
     kwargs: dict
+    pool: str | None = None
 
 
 def keyboard_interrupt(func):
@@ -45,6 +52,34 @@ def keyboard_interrupt(func):
         except KeyboardInterrupt:
             logger.info("Interrupted by user")
             # self.loop.stop()
+
+    return decorator
+
+
+def async_monitor(func):
+    @wraps(func)
+    async def decorator(*args, **kwargs):
+        t1 = time.time()
+        logger.debug("async_monitor")
+        result = await func(*args, **kwargs)
+
+        seconds = time.time() - t1
+        logger.debug("spending %s seconds", seconds)
+        return result
+
+    return decorator
+
+
+def sync_monitor(func):
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        t1 = time.time()
+        # logger.debug("monitor")
+        result = func(*args, **kwargs)
+
+        seconds = time.time() - t1
+        logger.debug("spending %s seconds", round(seconds, 5))
+        return result
 
     return decorator
 
@@ -71,6 +106,16 @@ class AsyncScheduler:
         
         """
 
+    @staticmethod
+    def define_pool(pool: str) -> ThreadPoolExecutor | ProcessPoolExecutor:
+        match pool:
+            case "thread":
+                return ThreadPoolExecutor()
+            case "process":
+                return ProcessPoolExecutor()
+            case _:
+                raise ValueError("Invalid pool type")
+
     # @keyboard_interrupt
     async def start(self) -> None:
         while self.jobs:
@@ -80,16 +125,27 @@ class AsyncScheduler:
             match job.type:
                 case "sync":
                     # await self.run_sync_job(job.func, args=job.args, kwargs={})
-                    await self.run_sync_job(job.func, args=job.args, kwargs=job.kwargs)
+                    # pool = ThreadPoolExecutor
+                    # pool = ProcessPoolExecutor
+                    # pool = None
+                    await self.run_sync_job(
+                        job.func,
+                        args=job.args,
+                        kwargs=job.kwargs,
+                        pool=AsyncScheduler.define_pool(job.pool),
+                    )
                 case "async":
-                    await self.run_async_job(job.func)
+                    await self.run_async_job(
+                        job.func,
+                        args=job.args,
+                        kwargs=job.kwargs,
+                    )
             # ...
             # check status jobs and run if ok
             await sleep(self.sleep_timer)
 
-    def add_sync_job(self, job: Callable, args: tuple, kwargs: dict) -> None:
-        logger.debug("adding sync job")
-        logger.debug(job)
+    def add_sync_job(self, job: Callable, args: tuple, kwargs: dict, pool: str) -> None:
+        logger.debug("adding sync job: %s", job)
         # calculate time this
         sync_job = Job(
             type="sync",
@@ -99,57 +155,81 @@ class AsyncScheduler:
             func=job,
             args=args,
             kwargs=kwargs,
+            pool=pool,
         )
-        logger.debug(sync_job)
-        import time
-
-        logger.debug(time.time())
         self.jobs.append(sync_job)
 
-    async def run_sync_job(self, job: Callable, args: tuple, kwargs: dict) -> None:
-        logger.debug("starting sync job")
-        from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+    def add_async_job(self, job: Coroutine, args: tuple, kwargs: dict) -> None:
+        logger.debug("adding async job: %s", job)
+        # calculate time this
+        async_job = Job(
+            type="async",
+            # when=self.loop.time() + 5,
+            when=10 + 5,
+            status="PENDING",
+            func=job,
+            args=args,
+            kwargs=kwargs,
+        )
+        self.jobs.append(async_job)
 
-        # pool = ThreadPoolExecutor
-        pool = ProcessPoolExecutor
-        # pool = None
+    async def run_sync_job(
+        self,
+        job: Callable,
+        args: tuple,
+        kwargs: dict,
+        pool: ThreadPoolExecutor | ProcessPoolExecutor,
+    ) -> None:
+        logger.debug("starting sync job")
         loop = get_event_loop()
-        t = loop.run_in_executor(pool(), job, *args, **kwargs)
+        t = loop.run_in_executor(pool, job, *args, **kwargs)
+        logger.debug("finished sync job")
         # task = create_task(t)
         # await task
         # r = await self.event_loop.run_in_executor(pool, job)
         # return r
 
-    async def add_async_job(self, job: Coroutine) -> None:
-        # self.jobs_.append(job)
-        # job_sleep_timer = min(
-        #     self.sleep_timer, self.jobs_[0].__await__() - self.event_loop.time()
-        # )  # We calculate the time until the next task and set the minimum value of Sleep Timer
-        pass
-
-    async def run_async_job(self, job: Coroutine) -> None:
+    async def run_async_job(
+        self,
+        job: Coroutine,
+        args: tuple,
+        kwargs: dict,
+    ) -> None:
+        logger.debug("starting async job")
+        await job(*args, **kwargs)
+        logger.debug("finished async job")
+        # loop = get_event_loop()
+        # loop.create_task(job, args, kwargs)
         # self.event_loop.call_at()
         # self.event_loop.time()
 
-        pass
 
-
-# event_loop.call_at()
-
-
-# if __name__ == '__main__':
-
-
+@sync_monitor
 def test(sleep_timer):
     import time
 
     time.sleep(sleep_timer)
-    print(f"hello world! {sleep_timer}")
+    print(f"sync hello world! {sleep_timer}")
+
+
+@async_monitor
+async def async_test(sleep_timer):
+    import asyncio
+
+    await asyncio.sleep(sleep_timer)
+    print(f"async hello world! {sleep_timer}")
 
 
 sc = AsyncScheduler()
-for i in range(1, 10):
+for i in range(1, 5):
     # sc.add_sync_job(test, args=(i,))
     # sc.add_sync_job(test, args=(i,), kwargs={"sleep_timer": i})
-    sc.add_sync_job(test, args=(3,), kwargs={})
+    sc.add_sync_job(test, args=(5,), kwargs={}, pool="thread")
+    sc.add_async_job(async_test, args=(5,), kwargs={})
+
+    # with cProfile.Profile() as pr:
+    # run(sc.start())
+
+    # pr.print_stats()
+# @async_monitor
 run(sc.start())
